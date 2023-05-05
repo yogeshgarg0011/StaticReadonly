295bb53025888d0c15c43ae303dd4ae1adca42b1
yogeshgarg0011 <125199173+yogeshgarg0011@users.noreply.github.com>
2023-05-05 09:18:34 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReadOnlyVariable
+{
+    internal class Program
+    {
+        public static readonly int i = 9;
+
+       static Program()
+        {
+            i=10;
+        }
+        static void Main(string[] args)
+        {
+            Program obj=new Program();
+            Console.WriteLine(i);
+        }
+    }
+}
